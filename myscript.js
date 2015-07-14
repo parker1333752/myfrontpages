c3d0ed62e703eb4c3d6795f90347adf20ee22bd6
lsj524993618 <524993618@qq.com>
2015-07-15 01:18:15 +0800

    2

--- myscript.js
+++ myscript.js
@@ -1,16 +1,7 @@
-var CANVAS_WIDTH = 200, CANVAS_HEIGHT = 400;
-var mycanvas , context;
-
-window.onload = function(){
-    createCanvas();
-}
-
-function createCanvas(){
-    document.body.innerHTML = "<canvas id=\"mycanvas\" height=\"" + CANVAS_HEIGHT + "\" width=\"" + CANVAS_WIDTH + " \"></canvas>";
-    mycanvas = document.getElementById("mycanvas");
-    context = mycanvas.getContext("2d");
-}
-
-function drawRect(){
-
-}
+$(document).ready(function(){
+    $('#btn').on("click", function(){
+        $.get("Server.php", {name:$("#namevalue").val()},function(data){
+            $("#result").text(data);
+        });
+    });
+});
